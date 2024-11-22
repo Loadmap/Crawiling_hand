80f742893fbb62820b93a0a32269f42100d12a6d
InHyeok Jang <dlsrhd95@gmail.com>
2024-11-22 22:32:28 +0900

    변경된 모든 파일 커밋

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import json
 import requests
 from collect_links import CollectLinks
@@ -6,11 +7,12 @@ from datetime import datetime
 
 class Crawling :
 
-    def __init__(self, n_threads = 4, crawl_google = True, crawl_naver = True, no_gui = False, save_path = 'download', limit = 10) :
+    def __init__(self, n_threads = 4, crawl_google = True, crawl_naver = True, no_gui = False, save_path = './download', limit = 10) :
         self.n_treads = n_threads
         self.crawl_google = crawl_google
         self.crawl_naver = crawl_naver
         self.no_gui = no_gui
+        self.save_path = save_path
         self.limit = limit = 1000 if limit == 0 else limit
         self.collect_links = CollectLinks
         self.browser_data, self.browser_keys = CollectLinks.read_json_browser()
@@ -61,6 +63,17 @@ class Crawling :
 
             return 'jpg'
 
+    @staticmethod
+    def save_files(response, file_path) :
+
+        try:
+
+            with open(f'{file_path}', 'wb') as f :
+                shutil.copyfileobj(response.raw, f)
+
+        except Exception as e :
+            print(f'저장 실패  : {e}')
+
     def make_data(self, browser, topic) :
         data = {}
         topics = self.make_and_read_topic()
@@ -90,6 +103,48 @@ class Crawling :
 
         return data
 
+    def download_imgs(self, browser, links, topic, max_cnt = 0) :
+        self.make_dir(f'{self.save_path}/{topic}')
+
+        img_topic = links[browser][0][0]
+        img_links = links[browser][0][1]
+
+        total = len(img_links)
+        success_cnt = 0
+
+        if max_cnt == 0 :
+            max_cnt = total
+
+        print(f'{browser} 로부터 다운로드 중')
+
+        for idx, link in enumerate(img_links) :
+
+            if success_cnt >= max_cnt :
+
+                break
+
+            try:
+
+                print(f'{success_cnt + 1} / {total} : {link}')
+
+                response = requests.get(link, stream=True, timeout=10)
+                ext = self.get_extension(link)
+
+                link_path = f'{self.save_path}/{topic}/{browser}_{str(idx).zfill(4)}'
+                link_ext_path = f'{link_path}.{ext}'
+
+                self.save_files(response, link_ext_path)
+
+                success_cnt += 1
+
+                del response
+
+                print('다운로드 완료')
+
+            except Exception as e :
+                print(f'예상치 못한 오류로 이미지 다운로드 실패 : {e}')
+
+        print('-' * 100)
 
     def download_from_browser(self, topic) :
 
@@ -112,7 +167,8 @@ class Crawling :
 
             return
 
+        self.download_imgs(browser, links, topic)
 
 if __name__ == '__main__' :
-    crawling = Crawling(n_threads = 4, crawl_google = True, crawl_naver = True, no_gui = False, limit = 5)
-    crawling.download_from_browser()
+    crawling = Crawling(n_threads = 4, crawl_google = True, crawl_naver = True, no_gui = False, save_path = './download', limit = 5)
+    crawling.download_from_browser(topic = 'hand')
